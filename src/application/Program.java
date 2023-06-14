3f2c51b1bb3ef283b512baea0dbbec6dad05ed2b
lucashenrique.info@gmail.com <lucashenrique.info@gmail.com>
2023-06-14 10:30:44 -0300

    implementando o programa principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,27 @@
 package application;
 
+import entities.Department;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o nome do departamento: ");
+        String departmentName = sc.nextLine();
+        System.out.print("Digite as informações do trabalhador:");
+        System.out.print("Nome: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.print("Salário base: ");
+        double baseSalary = sc.nextDouble();
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
 
+        sc.close();
     }
 }
